bd4ad5d0e2b922e5ca08873f189451931cf46ed3
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-04-10 15:49:41 +0900

    add credit

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
+/*
+    __  _______ ________              __
+   / / / / ___// ____/ /_  ___  _____/ /_
+  / /_/ /\__ \/ /   / __ \/ _ \/ ___/ __/
+ / __  /___/ / /___/ / / /  __(__  ) /_
+/_/ /_//____/\____/_/ /_/\___/____/\__/
+
+Credit: Rjlintkh (https://discord.com/channels/646456965983240212/814768666741964820/962398805167341618)
+
+*/
 
 import { events } from "bdsx/event";
 
-console.log('[HSChest] allocated');
+console.log("[HSChest] allocated");
 
-events.serverOpen.on(()=>{
-    console.log('[HSChest] launching');
+events.serverOpen.on(() => {
+    console.log("[HSChest] launching");
 });
 
-events.serverClose.on(()=>{
-    console.log('[HSChest] closed');
+events.serverClose.on(() => {
+    console.log("[HSChest] closed");
 });

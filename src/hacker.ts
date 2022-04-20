3a5e9adcb023e3cac47ee575291db19350d61629
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-04-20 21:54:24 +0900

    add `UpdateBlockPacket` constructor

--- src/hacker.ts
+++ src/hacker.ts
@@ -1,11 +1,12 @@
+import { BlockPos } from "bdsx/bds/blockpos";
 import { ItemStack } from "bdsx/bds/inventory";
-import { InventorySlotPacket } from "bdsx/bds/packets";
+import { InventorySlotPacket, UpdateBlockPacket } from "bdsx/bds/packets";
 import { pdb } from "bdsx/core";
 import { uint32_t, uint8_t } from "bdsx/nativetype";
 import { ProcHacker } from "bdsx/prochacker";
 
 const hacker = new ProcHacker({
-    ...pdb.getList("hacker.ini", {}, ["??0InventorySlotPacket@@QEAA@W4ContainerID@@IAEBVItemStack@@@Z"]),
+    ...pdb.getList("hacker.ini", {}, ["??0InventorySlotPacket@@QEAA@W4ContainerID@@IAEBVItemStack@@@Z", "??0UpdateBlockPacket@@QEAA@AEBVBlockPos@@IIE@Z"]),
 });
 
 export const InventorySlotPacket$InventorySlotPacket = hacker.js(
@@ -16,4 +17,15 @@ export const InventorySlotPacket$InventorySlotPacket = hacker.js(
     uint8_t,
     uint32_t,
     ItemStack,
+);
+
+export const UpdateBlockPacket$UpdateBlockPacket = hacker.js(
+    "??0UpdateBlockPacket@@QEAA@AEBVBlockPos@@IIE@Z",
+    UpdateBlockPacket,
+    null,
+    UpdateBlockPacket,
+    BlockPos,
+    uint32_t,
+    uint32_t,
+    uint8_t,
 );
